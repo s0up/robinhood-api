9759170fb53a3589400c92c8c6b1ebade89015c8
s0up <mike@haxt.net>
2017-08-09 11:09:40 -0700

    fix markets call

--- test.js
+++ test.js
@@ -5,6 +5,8 @@ const RobinHood = require('./index.js');
       let rh = new RobinHood();
 
       await rh.login({username: process.env.ROBINHOOD_USERNAME, password: process.env.ROBINHOOD_PASSWORD});
+
+      console.log(await rh.getMarkets());
    }catch(e){
       console.log('Error Code ' + e.code);
       console.log(e);
